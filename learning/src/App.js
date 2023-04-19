fdb63bd0baf2b48cbdfa3cca28f41181b172ad91
soonh1 <johnny_tran_3@hotmail.com>
2023-04-19 15:46:55 +0200

    changes

--- learning/src/App.js
+++ learning/src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
     ]
     return (
       <div className="container">
-        <Table characterData={characters} removeCharacter={this.removeCharacter}/>
-        <Form handleSubmit={ this.handleSubmit }/>
+        {/* <Table characterData={characters} removeCharacter={this.removeCharacter}/>
+        <Form handleSubmit={ this.handleSubmit }/> */}
         <h1>Plan A</h1>
         <List listData={listItem}/>
       </div>
